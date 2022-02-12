fdb6987d67a9f41277ec68241d726e646f0f83cb
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2022-02-12 16:50:16 +0100

    Fall fix

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/NoFallListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/NoFallListener.java
@@ -1,21 +1,23 @@
 package com.gmail.val59000mc.scenarios.scenariolisteners;
 
+import com.gmail.val59000mc.game.GameManager;
+import com.gmail.val59000mc.game.GameState;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-public class NoFallListener extends ScenarioListener{
+public class NoFallListener extends ScenarioListener {
 
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent e) {
         if (e.getEntity() instanceof Player) {
-
             if (e.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
-                e.setCancelled(true);
+                if (GameManager.getGameManager().getGameState().equals(GameState.PLAYING) && !GameManager.getGameManager().getPvp()) {
+                    e.setCancelled(true);
+                }
             }
 
         }
     }
-
 }
